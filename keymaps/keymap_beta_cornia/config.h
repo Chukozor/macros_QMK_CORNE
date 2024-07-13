2307c28f0261308c4cd62a2cf94b7fe9c8cfe678
Chukozor <9273715+Chukozor@users.noreply.github.com>
2024-07-14 00:51:18 +0200

    allume de façon symétrique au master keyboard

--- keymaps/keymap_beta_cornia/config.h
+++ keymaps/keymap_beta_cornia/config.h
@@ -25,7 +25,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Select hand configuration */
 
 // #define MASTER_LEFT
-#define EE_HANDS
+#define MASTER_RIGHT
+
+#define SPLIT_LAYER_STATE_ENABLE
+
+
+// #define EE_HANDS
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
