a04bd4be3f076c4cc593b7ced5cd9133266ce3ad
Chukozor <9273715+Chukozor@users.noreply.github.com>
2024-07-16 01:55:33 +0200

    ajoud des settings vscode

--- keymaps/keymap_beta_cornia/z_code_of_the_net/leds_should_work_with_brightness-discord_.h
+++ keymaps/keymap_beta_cornia/z_code_of_the_net/leds_should_work_with_brightness-discord_.h
@@ -3,12 +3,12 @@ bool rgb_matrix_indicators_user(void) {
   RGB rgb; 
   #ifdef RGB_MATRIX_ENABLE
     switch (get_highest_layer(layer_state 1 | default_layer_state)) {
-      case RAISE:
+      case _OTHER_GAME:
         hsv = (HSV){255, 255, rgb_matrix_get_val()};
         rgb = hsv_to_rgb(hsv);
         rgb_matrix_set_color_all(rgb.r, rgb.g, rgb.b);
         break;
-      case LOWER:
+      case _CAPS_LOCK:
         hsv = (HSV){170, 255, rgb_matrix_get_val()};
         rgb = hsv_to_rgb(hsv);
         rgb_matrix_set_color_all(rgb.r, rgb.g, rgb.b);
