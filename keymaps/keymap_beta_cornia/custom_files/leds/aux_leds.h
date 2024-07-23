cdb0a921fede3d1dc7e412670198274218dfddd7
Chukozor <9273715+Chukozor@users.noreply.github.com>
2024-07-23 04:13:41 +0200

    Ajout de couleurs selon les couches

--- keymaps/keymap_beta_cornia/custom_files/leds/aux_leds.h
+++ keymaps/keymap_beta_cornia/custom_files/leds/aux_leds.h
@@ -74,15 +74,21 @@ bool rgb_matrix_indicators_user(void) {
       case _MOUSE_LAYER:
         hsv = (HSV){140, 255, rgb_matrix_get_val()};
         rgb = hsv_to_rgb(hsv);
-        int j = 33;
         for (int i = 6; i <= 26; ++i) {
           // rgb_matrix_set_color(index_deplacement_gaming[i], rgb.r, rgb.g, rgb.b);
           rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
-          rgb_matrix_set_color(j, rgb.r, rgb.g, rgb.b);
-          ++j;
+          rgb_matrix_set_color(i+27, rgb.r, rgb.g, rgb.b);
         }
         break;
-
+      case _CAPS_LOCK:
+        hsv = (HSV){215, 255, rgb_matrix_get_val()};
+        rgb = hsv_to_rgb(hsv);
+        for (int i = 6; i <= 26; ++i) {
+          // rgb_matrix_set_color(index_deplacement_gaming[i], rgb.r, rgb.g, rgb.b);
+          rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
+          rgb_matrix_set_color(i+27, rgb.r, rgb.g, rgb.b);
+        }
+        break;
     }
   #endif
   return false;
